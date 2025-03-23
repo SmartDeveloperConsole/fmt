93628f1714f8a9aed20616d9059f70e0b236d078
Harsh <smartdeveloperconsole1@gmail.com>
2025-03-23 12:50:20 +0530

    base_migration

---
+++ app/Http/Controllers/FamilyMemberController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\family_member;
+use Illuminate\Http\Request;
+
+class FamilyMemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(family_member $family_member)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(family_member $family_member)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, family_member $family_member)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(family_member $family_member)
+    {
+        //
+    }
+}
---
+++ app/Models/family_member.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class family_member extends Model
+{
+    //
+}
